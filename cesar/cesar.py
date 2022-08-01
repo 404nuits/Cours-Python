06b592086beeaef8185c27652b24ad6a8c6572b1
404nuits <56111337+404nuits@users.noreply.github.com>
2022-08-01 19:22:50 +0200

    Update cesar.py

--- cesar/cesar.py
+++ cesar/cesar.py
@@ -1,7 +1,19 @@
 def encode(chaine, decalage):
+    
+    debut = 97
+    fin = 122
+    
     new_chaine = ""
     for lettre in chaine:
-        new_chaine += chr(ord(lettre) + decalage)
+        temp1 = ord(lettre)
+        
+        decale = temp1 + decalage
+        
+        if decale > fin:
+            decale = decale - 26
+        
+        temp2 = chr(decale)
+        new_chaine += temp2
     return new_chaine
 
 def decode(chaine, decalage):
